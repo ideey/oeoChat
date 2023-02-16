c699caf10acd39ee2a5835b3d9523f130ba91657
jy00566722 <jy00566723@gmail.com>
2023-02-17 00:43:19 +0800

    f

--- server/service/openai/temegram.go
+++ server/service/openai/temegram.go
@@ -17,13 +17,13 @@ func (telegramService *TelegramService) SendMessage(message openai.SendMessageTo
 	// url := "https://api.telegram.org/bot6250735595:AAF8GxRVi0wSY734CLoJd-3GbT2Yof-gdOA/sendMessage"
 	url := "https://api.telegram.org/bot" + global.TELEGRAM_TOKEN + "/sendMessage"
 	client := resty.New()
-	resp, err := client.R().
+	_, err := client.R().
 		SetHeader("Content-Type", "application/json").
 		SetBody(message).
 		Post(url)
 	if err != nil {
 		fmt.Printf("SendMessage出错了:err: %v\n", err)
 	}
-	s := string(resp.Body())
-	fmt.Printf("发信息后返回的数据:s: %v\n", s)
+	// s := string(resp.Body())
+	// fmt.Printf("发信息后返回的数据:s: %v\n", s)
 }
